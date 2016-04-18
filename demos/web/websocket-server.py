dca2c50e00c4ed67000a9cb70696b797126e3a0c
Nathan Anaman <nanaman@solstice-consulting.com>
2016-04-18 11:23:54 -0500

    test

--- demos/web/websocket-server.py
+++ demos/web/websocket-server.py
@@ -87,6 +87,48 @@ class Face:
         )
 
 
+# http://stackoverflow.com/questions/3488934/simplejson-and-numpy-array/24375113#24375113
+'''
+    Usage:
+    expected = np.arange(100, dtype=np.float)
+    dumped = json.dumps(expected, cls=NumpyEncoder)
+    result = json.loads(dumped, object_hook=json_numpy_obj_hook)
+'''
+
+
+class NumpyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        """If input object is an ndarray it will be converted into a dict
+        holding dtype, shape and the data, base64 encoded.
+        """
+        if isinstance(obj, np.ndarray):
+            if obj.flags['C_CONTIGUOUS']:
+                obj_data = obj.data
+            else:
+                cont_obj = np.ascontiguousarray(obj)
+                assert(cont_obj.flags['C_CONTIGUOUS'])
+                obj_data = cont_obj.data
+            data_b64 = base64.b64encode(obj_data)
+            return dict(__ndarray__=data_b64,
+                        dtype=str(obj.dtype),
+                        shape=obj.shape)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder(self, obj)
+
+
+def json_numpy_obj_hook(dct):
+    """Decodes a previously encoded numpy ndarray with proper shape and dtype.
+
+    :param dct: (dict) json encoded ndarray
+    :return: (ndarray) if input was an encoded ndarray
+    """
+    if isinstance(dct, dict) and '__ndarray__' in dct:
+        data = base64.b64decode(dct['__ndarray__'])
+        return np.frombuffer(data, dct['dtype']).reshape(dct['shape'])
+    return dct
+
+
 class OpenFaceServerProtocol(WebSocketServerProtocol):
 
     def __init__(self):
@@ -229,7 +271,7 @@ class OpenFaceServerProtocol(WebSocketServerProtocol):
         msg = {
             "type": "TRAINING_PROCESSING",
             "count": len(self.images),
-            "image_data": json.dumps(d)
+            "image_data": json.dumps(d, cls=NumpyEncoder)
         }
         self.sendMessage(json.dumps(msg))
 
